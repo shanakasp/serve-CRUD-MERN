40f105d054aa5c22adefa39d7bf23ddaa6571192
shanakasp <74164029+shanakasp@users.noreply.github.com>
2023-12-29 21:38:02 +0530

    added 1810 files

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const UserModel = require("./models/Users");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+try {
+  mongoose.connect("mongodb://127.0.0.1:27017/crud", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  const db = mongoose.connection;
+
+  db.on("error", (error) => {
+    console.error("MongoDB connection error:", error);
+  });
+
+  db.once("open", () => {
+    console.log("Connected to MongoDB");
+  });
+} catch (error) {
+  console.error("Error connecting to MongoDB:", error);
+}
+app.post("/createUser", (req, res) => {
+  app.post("/createUser", (req, res) => {
+    UserModel.create(req.body)
+      .then((users) => res.json(users))
+      .catch((err) => res.json(err));
+  });
+});
+
+app.listen(3001, () => {
+  console.log("Server Running");
+});
